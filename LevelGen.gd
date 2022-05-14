dfb27920c9967823ad3be35c93c36ca50f48df2d
Jacob Artis <zjac219@live.rhul.ac.uk>
2022-05-15 00:55:06 +0100

    Some final late night changes mainly working on adding different types of rooms
    Signed-off-by: Jacob Artis <zjac219@live.rhul.ac.uk>

--- LevelGen.gd
+++ LevelGen.gd
@@ -5,8 +5,12 @@
 extends Spatial
 
 const CELL = preload("res://Map Resources/Rooms/Cell.tscn")
+const CELL2 = preload("res://Map Resources/Rooms/Cell2.tscn")
 
 export var mapScene: PackedScene
+export var tileSet: TileSet
+
+onready var roomList: = [CELL, CELL2]
 
 var cells = []
 
@@ -17,15 +21,20 @@ func generate_map():
 	if !mapScene is PackedScene: return
 	var map = mapScene.instance()
 	var tileMap = map.get_tilemap()
-	var used_tiles = tileMap.get_used_cells()
-	map.free() # We don't need it now that we have the tile data
-	for tile in used_tiles:
-		var cell = CELL.instance()
-		add_child(cell)
-		cell.translation = Vector3(tile.x*Global.GRID_SIZE, 0, tile.y*Global.GRID_SIZE)
-		cells.append(cell)
-	for cell in cells:
-		cell.update_faces(used_tiles)
+	var tileId: int = 0
+	
+	for tileId in range(tileSet.get_tiles_ids().size()):
+		
+		var used_tiles = tileMap.get_used_cells_by_id(tileId)
+		
+		for tile in used_tiles:
+			var cell = roomList[tileId-1].instance()
+			add_child(cell)
+			cell.translation = Vector3(tile.x*Global.GRID_SIZE, 0, tile.y*Global.GRID_SIZE)
+			cells.append(cell)
+		
+		for cell in cells:
+			cell.update_faces(used_tiles)
 
 
 
--- Map/GenerateRandMap.gd
+++ Map/GenerateRandMap.gd
@@ -1,9 +1,51 @@
 extends TileMap
 
-export var rooms: int = 10
+export var rooms: int = 20
+export var tileSet: TileSet
 
 var rand = RandomNumberGenerator.new()
 var avalibleRooms = []
+var currentRooms = []
 
+#Randomly generates the level for the set number or rooms
 func generate():
+	var x: int = 0
 	set_cell(0,0,1)
+	currentRooms.append([0,0])
+	updateAvalible()
+	
+	for x in range(rooms):
+		rand.randomize()
+		var tileId: int = rand.randi_range(1,tileSet.get_tiles_ids().size())
+		addRoom(avalibleRooms[rand.randi_range(0,avalibleRooms.size()-1)][0],tileId)
+		updateAvalible()
+
+#Adds a room to the generation and adds that room to current rooms
+func addRoom(currentRoom, tileId):
+	set_cell(avalibleRooms[currentRoom][0],avalibleRooms[currentRoom][1],tileId)
+	currentRooms.append(avalibleRooms[currentRoom])
+
+#Resets then adds all avalible rooms to the array
+func updateAvalible():
+	var z: int = 0
+	
+	avalibleRooms.clear()
+	
+	for z in range(currentRooms.size()):
+		
+		var currentRoom = currentRooms[z]
+		addNoDups([(currentRoom[0]-1),(currentRoom[1])])
+		addNoDups([(currentRoom[0]),(currentRoom[1]-1)])
+		addNoDups([(currentRoom[0]+1),(currentRoom[1])])
+		addNoDups([(currentRoom[0]),(currentRoom[1]+1)])
+
+#Adds a room to the avalible rooms array that isn't in any other array
+func addNoDups(currentRoom):
+	
+	if avalibleRooms.has(currentRoom):
+		return
+	
+	if currentRooms.has(currentRoom):
+		return
+	
+	avalibleRooms.append(currentRoom)
